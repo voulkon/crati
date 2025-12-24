372fa678defa0daddc9a64675030c370e93baee2
voulkon <voulkon93@gmail.com>
2025-12-25 00:25:42 +0200

    Update environment variable for Django secret key in settings.py

--- backend/diavgeia_project/settings.py
+++ backend/diavgeia_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 default_unsafe_secret_key = "django-insecure-_qy*f$4zd#u_)lt_v%2t*29g9f$rr0=u!-g=t#+6g+z0)u6zsi"
-SECRET_KEY = os.getenv("SECRET_KEY", default_unsafe_secret_key) 
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", default_unsafe_secret_key) 
 
 
 
